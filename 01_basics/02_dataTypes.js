08a8b82c6869abbf63da8887b91c306513c1fe5c
Aditya Chaudhuri <adityachaudhuri26@gmail.com>
2026-09-17 05:38:55 +0000

    datatypes 01

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    + 3);
+    //code readability is mandatory so avoid complexity
+
+console.log("Crazy Adi?");
+
+let name = "adi"
+let age = 20
+let isLogged = true
+
+// number => 2 to power 53
+// bigint // used to work with huge numbers used in stocks or facebook
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof null); // object as output
+console.log(typeof undefined); // undefined as output
